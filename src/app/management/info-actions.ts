29fbf2372f220440ad0f0f53b0c19a75e57a74f3
jaDEVirek <devirekcoding@gmail.com>
2022-04-23 23:15:55 +0200

    updating in actions for reducer

--- src/app/management/info-actions.ts
+++ src/app/management/info-actions.ts
@@ -1,10 +1,15 @@
 import { createAction, props } from '@ngrx/store';
+import { Message } from '../model/message';
 
-export const addInfo = createAction('[Information Component] Add Info',
-props<{ message: string }>()
+export const addInfo = createAction(
+  '[Information Component] Add Info',
+  props<{ message: string }>()
 );
 export const deleteInfo = createAction(
   '[Information Component] Delete Info',
   props<{ key: string }>()
 );
-export const updateInfo = createAction('[Information Component] Update Info');
+export const updateInfo = createAction(
+  '[Information Component] Update Info',
+  props<{ message: Message }>()
+);
--- src/app/management/info_reducer.ts
+++ src/app/management/info_reducer.ts
@@ -2,20 +2,27 @@ import { createReducer, on } from '@ngrx/store';
 import { Message } from '../model/message';
 import { addInfo, deleteInfo, updateInfo } from './info-actions';
 export interface InfoMessageState {
-  messages: Message[],
-  status: 'pending' | 'loading' | 'error' | 'sucess',
+  messages: Message[];
+  status: 'pending' | 'loading' | 'error' | 'sucess';
 }
 
-export const initialState: InfoMessageState =  {
-    messages: [],
-    status: 'pending',
+export const initialState: InfoMessageState = {
+  messages: [],
+  status: 'pending',
 };
 
 export const informationState = createReducer(
   initialState,
-  on(addInfo, (state,{message} ) => ({
-    ...state, messages: [...state.messages, { id: Date.now(), text: message}],
+  on(addInfo, (state, { message }) => ({
+    ...state,
+    messages: [...state.messages, { id: Date.now(), text: message }],
   })),
-  on(deleteInfo, (state) => state - 1),
-  on(updateInfo, (state) => 0)
+  on(deleteInfo, (state, { key }) => ({
+    ...state,
+    messages: state.messages.filter((info) => info.id != Number(key)),
+  })),
+  on(updateInfo,  (state, { message }) => ({
+    ...state,
+    messages: state.messages.filter((info) => info.id != Number(key)),
+  })),
 );
