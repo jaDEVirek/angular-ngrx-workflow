74dc7326d7a4b97763c9c7a50e4b43fcc6dded40
Wiktor Religo <wiciorny@gmail.com>
2022-05-01 23:01:58 +0200

    adding neccessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import {MessagesPageComponent} from './components/messages-page/messages-page.co
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatListModule} from "@angular/material/list";
+import {FormsModule} from "@angular/forms";
+import {StoreModule} from "@ngrx/store";
+import {messageInformationState} from "./management/message_reducer";
 
 @NgModule({
-    imports: [BrowserModule, MatInputModule, MatButtonModule, MatListModule],
+    imports: [BrowserModule, MatInputModule, FormsModule, MatButtonModule, MatListModule,  StoreModule.forRoot({ messages: messageInformationState })],
     declarations: [AppComponent, MessagesPageComponent],
     bootstrap: [AppComponent],
 })
